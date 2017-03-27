9c0b7456c1cd2072041a04f0cc9a032b6b51119b
newkyu <newkyu12@gmail.com>
2017-03-28 00:43:48 +0900

    Update MusicRadioPlayer.py

--- MusicRadioPlayer.py
+++ MusicRadioPlayer.py
@@ -7,14 +7,19 @@ song = ['Closer.mp3','Shape Of You.mp3','Starving.mp3','The Ocean.mp3','We Don\'
 cad = pifacecad.PiFaceCAD()
 cad.lcd.backlight_on()
 mixer.init()
-curNum = 0
 
+play = True
+mute = False
+curSong = 0
+volume = mixer.music.get_volume()
 while True:
-	if curNum == len(song):
-		curNum = 0
+	if curSong == len(song):
+		curSong = 0
 	cad.lcd.clear()
-	mixer.music.load(song[curNum])
-	cad.lcd.write(song[curNum])
+	cad.lcd.blink_off()
+	cad.lcd.cursor_off()
+	mixer.music.load(song[curSong])
+	cad.lcd.write(song[curSong])
 	mixer.music.play()
 	
 	while mixer.music.get_busy() == True:
@@ -22,4 +27,46 @@ while True:
 		nowTime = now.strftime('%H:%M')
 		cad.lcd.set_cursor(10,1)
 		cad.lcd.write(nowTime)
-		continue
+		
+		# song control
+		if cad.switches[1].value:	# prev song
+			sleep(0.3)
+			curSong-=2
+			if curSong==-2:
+				curSong=len(song)-2
+			break;
+		if cad.switches[3].value:	# next song
+			sleep(0.3)
+			break;
+			
+		if cad.switches[2].value:	# play and pause
+			sleep(0.2)
+			if play:
+				mixer.music.pause()
+				play = False
+			else:
+				mixer.music.unpause()
+				play = True
+				
+		# volume control
+		if cad.switches[6].value:	# volume down
+			sleep(0.1)
+			volume -= 0.05
+			if volume < 0:
+				volume = 0
+			mixer.music.set_volume(volume)
+		if cad.switches[7].value:	# volume up
+			sleep(0.1)
+			volume += 0.05
+			if volume > 1:
+				volume = 1
+			mixer.music.set_volume(volume)
+		if cad.switches[5].value:
+			sleep(0.2)
+			if mute:
+				mixer.music.set_volume(volume)
+				mute = False
+			else:
+				mixer.music.set_volume(0)
+				mute = True
+	curSong+=1
