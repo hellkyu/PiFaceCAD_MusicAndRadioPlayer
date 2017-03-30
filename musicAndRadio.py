b8675c5b8870a335b3962dd1422347d0578b27bc
newkyu <newkyu12@gmail.com>
2017-03-31 02:42:43 +0900

    Update musicAndRadio.py

--- musicAndRadio.py
+++ musicAndRadio.py
@@ -4,7 +4,6 @@ from time import sleep
 import datetime
 import threading
 import os
-import subprocess
 
 
 cad = pifacecad.PiFaceCAD()
@@ -12,8 +11,23 @@ cad.lcd.backlight_on()
 cad.lcd.blink_off()
 cad.lcd.cursor_off()
 
+# bitmap
+bmp_note = pifacecad.LCDBitmap([2,3,2,2,14,30,12,0])
+bmp_play = pifacecad.LCDBitmap([0,8,12,14,12,8,0,0])
+bmp_pause = pifacecad.LCDBitmap([0,27,27,27,27,27,0,0])
+bmp_clock = pifacecad.LCDBitmap([0,14,21,23,17,14,0,0])
+bmp_vol1 = pifacecad.LCDBitmap([1,3,15,15,15,3,1,0])
+bmp_vol2 = pifacecad.LCDBitmap([8,16,0,24,0,16,8,0])
+
+cad.lcd.store_custom_bitmap(0, bmp_note)
+cad.lcd.store_custom_bitmap(1, bmp_play)
+cad.lcd.store_custom_bitmap(2, bmp_pause)
+cad.lcd.store_custom_bitmap(3, bmp_clock)
+cad.lcd.store_custom_bitmap(4, bmp_vol1)
+cad.lcd.store_custom_bitmap(5, bmp_vol2)
+
 # music
-song = ['Closer.mp3','Shape Of You.mp3','Starving.mp3','The Ocean.mp3','We Don\'t Talk Anymore.mp3']
+song = ['Closer','Shape Of You','Starving','The Ocean','We Don\'t Talk Anymore']
 
 # radio
 path = '/tmp/mplayer-control'
@@ -25,7 +39,9 @@ channelName = ["MBC", "SBS","SeoulFBEC","YeosooMBC","JIBS","TBN"]
 
 pin = -1
 remote = -1
-
+volume = 50
+play = True
+mute = False
 
 def update_pin_text(event):
 	 global pin
@@ -39,33 +55,70 @@ def showTime():
 	now = datetime.datetime.now()
 	nowTime = now.strftime('%H:%M')
 	cad.lcd.set_cursor(10,1)
+	cad.lcd.write_custom_bitmap(3)
 	cad.lcd.write(nowTime)
 	cad.lcd.set_cursor(0,0)
-
+	
+def writeVolume():
+	global volume
+	global mute
+	cad.lcd.set_cursor(0,1)
+	cad.lcd.write_custom_bitmap(4)
+	cad.lcd.write_custom_bitmap(5)
+	if mute:
+		cad.lcd.write('mute')
+	else:
+		cad.lcd.write(str(volume).rjust(4))
+	
+def updateVolume():
+	global volume
+	cad.lcd.set_cursor(2,1)
+	if mute:
+		cad.lcd.write('mute')
+	else:
+		cad.lcd.write(str(volume).rjust(4))
+	
+	
+def writeBottomLine():
+	global play
+	showTime()
+	writeVolume()
+	cad.lcd.set_cursor(7,1)
+	if play:
+		cad.lcd.write_custom_bitmap(1)
+	else:
+		cad.lcd.write_custom_bitmap(2)
+	cad.lcd.set_cursor(0,0)
+	
+	
+	
+	
 def musicMainfunc():
 	global pin
 	global remote
+	global volume
+	global play
+	global mute
 	
-	play = True
-	mute = False
 	curSong = 0
 	pin = -1
 	remote = -1
 	
 	mixer.init()
-	volume = mixer.music.get_volume()
+	mixer.music.set_volume(float(volume)/100)
 	while True:
 		if curSong == len(song):
 			curSong = 0
 		cad.lcd.clear()
 		cad.lcd.blink_off()
 		cad.lcd.cursor_off()
-		mixer.music.load(song[curSong])
+		mixer.music.load(song[curSong]+'.mp3')
+		cad.lcd.write_custom_bitmap(0)
 		cad.lcd.write(song[curSong])
 		mixer.music.play()
 		
 		while mixer.music.get_busy() == True:
-			showTime()
+			writeBottomLine()
 			
 			# song control
 			if pin == 1 or remote == 1:	# prev song
@@ -93,29 +146,33 @@ def musicMainfunc():
 			# volume control
 			elif pin == 6 or remote == 6:	# volume down
 				while True:
-					volume -= 0.05
+					volume -= 3
+					print(volume)
 					if volume < 0:
 						volume = 0
-					mixer.music.set_volume(volume)
-					sleep(0.1)
+					mixer.music.set_volume(float(volume)/100)
+					updateVolume();
+					sleep(0.2)
 					if not cad.switches[6].value:
 						break
-					pin = -1
-					remote = -1
+				pin = -1
+				remote = -1
 			elif pin == 7 or remote == 7:	# volume up
 				while True:
-					volume += 0.05
-					if volume > 1:
-						volume = 1
-					mixer.music.set_volume(volume)
-					sleep(0.1)
+					volume += 3
+					print(volume)
+					if volume > 100:
+						volume = 100
+					mixer.music.set_volume(float(volume)/100)
+					updateVolume();
+					sleep(0.2)
 					if not cad.switches[7].value:
 						break
 				pin = -1
 				remote = -1
 			elif pin == 5 or remote == 5:	# mute
 				if mute:
-					mixer.music.set_volume(volume)
+					mixer.music.set_volume(float(volume)/100)
 					mute = False
 				else:
 					mixer.music.set_volume(0)
@@ -133,61 +190,99 @@ def musicMainfunc():
 def radioMainFunc():
 	global pin
 	global remote
-	curNum = 0
+	global volume
+	global play
+	global mute
+	
+	f = open(path,'w')
+	if mute:
+		f.write('volume 0 1\n')
+	else:
+		f.write('volume '+ str(volume) +' 1\n')
+	f.close()
+	
+	play = True;
+	curCh = 0
 	cad.lcd.clear()
-	showTime()
-	cad.lcd.write('CH : '+channelName[curNum])
+	writeBottomLine()
+	cad.lcd.write('CH : '+channelName[curCh])
+
 	while True:
 		showTime()
 		# play control
-		if pin in [2, 5] or remote in [2, 5]:		# pause and unpause and mute
-			subprocess.call('echo \"pause\" > /tmp/mplayer-control', shell=True)
+		if pin == 2 or remote == 2:		# pause and unpause
+			os.system('echo \"pause\" > /tmp/mplayer-control')
+			if play:
+				play = False;
+			else:
+				play = True;
+			writeBottomLine()
 			pin = -1
 			remote = -1
 		elif pin == 3 or remote == 3:	# next channel
-			curNum = (curNum + 1) % len(channelLink)
-			subprocess.call('echo \"loadfile '+channelLink[curNum] +'\" > /tmp/mplayer-control', shell=True)
+			curCh = (curCh + 1) % len(channelLink)
+			os.system('echo \"loadfile '+channelLink[curCh] +'\" > /tmp/mplayer-control')
 			cad.lcd.clear()
-			showTime()
-			cad.lcd.write('CH : '+channelName[curNum])
+			writeBottomLine()
+			cad.lcd.write('CH : '+channelName[curCh])
 			pin = -1
 			remote = -1
 		elif pin == 1 or remote == 1:	# prev channel
-			if curNum == 0:
-				curNum = len(channelLink)-1
+			if curCh == 0:
+				curCh = len(channelLink)-1
 			else:
-				curNum -= 1
-			subprocess.call('echo \"loadfile '+channelLink[curNum] +'\" > /tmp/mplayer-control', shell=True)
+				curCh -= 1
+			os.system('echo \"loadfile '+channelLink[curCh] +'\" > /tmp/mplayer-control')
 			cad.lcd.clear()
-			showTime()
-			cad.lcd.write('CH : '+channelName[curNum])
+			writeBottomLine()
+			cad.lcd.write('CH : '+channelName[curCh])
 			pin = -1
 			remote = -1
 		# volume control
 		elif pin == 6 or remote == 6:	# volume down
 			while True:
-				subprocess.call('echo \"volume -1\" > /tmp/mplayer-control', shell=True)
-				sleep(0.1)
+				volume -= 3
+				if volume < 0:
+					volume = 0
+				os.system('echo \"volume '+ str(volume) +' 1\" > /tmp/mplayer-control')
+				updateVolume();
+				sleep(0.2)
 				remote = -1
 				if not cad.switches[6].value:
 					break
 			pin = -1
 		elif pin == 7 or remote == 7:	# volume up
 			while True:
-				subprocess.call('echo \"volume 1\" > /tmp/mplayer-control', shell=True)
-				sleep(0.1)
+				volume += 3
+				if volume > 100 :
+					volume = 100
+				os.system('echo \"volume '+ str(volume) +' 1\" > /tmp/mplayer-control')
+				updateVolume();
+				sleep(0.2)
 				remote = -1
 				if not cad.switches[7].value:
 					break
 			pin = -1
+		elif pin == 5 or remote == 5:	# mute
+			if mute:
+				os.system('echo \"volume '+ str(volume) +' 1\" > /tmp/mplayer-control')
+				mute = False;
+			else:
+				os.system('echo \"volume 0 1\" > /tmp/mplayer-control')
+				mute = True;
+			updateVolume();
+			pin = -1
+			remote = -1
 		elif pin == 4 or remote == 4: 
 				pin = -1
 				remote = -1
-				subprocess.call('echo \"quit\" > /tmp/mplayer-control', shell=True)
+				os.system('echo \"quit\" > /tmp/mplayer-control')
 				return
 			
 
 def mainFunc():
+	global volume
+	
 	listener = pifacecad.SwitchEventListener(chip=cad)
 	for i in range(8):
 		listener.register(i, pifacecad.IODIR_FALLING_EDGE, update_pin_text)
@@ -201,18 +296,13 @@ def mainFunc():
 	#musicMain = threading.Thread(target=musicMainfunc)
 	radioMain = threading.Thread(target=radioMainFunc)
 	while True:
-		os.system('rm '+path)
-		os.system('mkfifo '+path)
 		musicMainfunc()
 		radioMain = threading.Thread(target=radioMainFunc)
+		os.remove(path)
+		os.mkfifo(path)
 		radioMain.start()
 		os.system('mplayer -slave -input file='+path+' '+channelLink[0])
 		radioMain.join()
 	
 	
 mainFunc()
-			
-
-
-
-
